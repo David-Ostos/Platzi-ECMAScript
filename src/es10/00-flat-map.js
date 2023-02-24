e8652285e367b54ec377343381f0a7fe0f81135b
David Ostos <davidostos2@gmail.com.com>
2023-02-24 14:34:24 -0400

    terminando clase18

--- src/es10/00-flat-map.js
+++ src/es10/00-flat-map.js
@@ -1,8 +1,12 @@
 //flat
 const array = [1,1,2,3,4 ,[1,3,4,[1,2,4]]];
+const newArray = array.flat(2);
 
-console.log(array);
-console.log(array.flat(2));
+
+console.log(array[5][3]);
+console.log(newArray);
+
+console.log(newArray[10]);
 
 // flatmap
 const array2 = [1,2,3,4];
