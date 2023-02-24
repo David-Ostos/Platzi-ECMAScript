3e1534a29102f9ecba62f86329d9b3958cdc05e3
David Ostos <davidostos2@gmail.com.com>
2023-02-24 18:59:15 -0400

    terminando clase24

---
+++ src/es11/06-dynamic-import.js
@@ -0,0 +1,9 @@
+
+
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', async function() {
+    const module = await import('./module.js')
+    console.log(module)
+    module.hello()
+})
